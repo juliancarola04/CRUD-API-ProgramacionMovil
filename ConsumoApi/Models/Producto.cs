2a4c1697db002c98b84142cfc3afa733ee1ce9ce
Julián Carola <juliancarola04@gmail.com>
2026-06-06 16:31:02 -0300

    Agregar primera idea de como mostrar los productos.

---
+++ ConsumoApi/Models/Producto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumoApi.Models
+{
+    public class Producto
+    {
+        public int id { get; set; }
+        public string title { get; set; }
+        public float price { get; set; }
+        public string description { get; set; }
+        public string category { get; set; }
+        public string image { get; set; }
+        public Rating rating { get; set; }
+    }
+}
---
+++ ConsumoApi/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsumoApi.Models
+{
+    public class Rating
+    {
+        public float rate { get; set; }
+        public int count { get; set; }
+    }
+}

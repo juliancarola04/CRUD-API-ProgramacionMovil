6c7a80625387fb9d5712ab64ff0e0f54ba4aef51
Julián Carola <juliancarola04@gmail.com>
2026-06-09 22:41:35 -0300

    Agregar validaciones client-side y eliminar visualmente.

--- ConsumoApi/Models/Producto.cs
+++ ConsumoApi/Models/Producto.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ConsumoApi.Models
@@ -7,11 +8,13 @@ namespace ConsumoApi.Models
     public class Producto
     {
         public int id { get; set; }
+        [Required(ErrorMessage = "Es requerido un nombre para el artículo.")]
         public string title { get; set; }
+        [Required(ErrorMessage = "Es requerido un precio.")]
         public float price { get; set; }
         public string description { get; set; }
         public string category { get; set; }
         public string image { get; set; }
-        public Rating rating { get; set; }
+        public Rating rating { get; set; } = new Rating();
     }
 }
--- ConsumoApi/Models/Rating.cs
+++ ConsumoApi/Models/Rating.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ConsumoApi.Models
 {
     public class Rating
     {
+        [Range(0, 5, ErrorMessage = "El puntaje debe estar entre 0 y 5.")]
         public float rate { get; set; }
         public int count { get; set; }
     }
